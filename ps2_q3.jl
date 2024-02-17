2f021bd0f48aea96982bf0640ba4a4596fc18e59
Diego S Cardoso <cardosod@purdue.edu>
2024-02-16 20:39:24 -0500

    Create ps2_q3.jl

---
+++ ps2_q3.jl
@@ -0,0 +1,18 @@
+# The code below creates fake data for our estimation
+using Random
+
+Random.seed!(652) # Set a random seed for reproducibility
+
+# Set true parameter values
+β0_true = 2
+β1_true = 3
+
+# Data Generation
+n = 1000
+x = rand(n) * 10  # Uniform distribution
+ϵ = randn(n)  # Normally distributed errors
+y = β0_true .+ β1_true .* x .+ ϵ
+
+#######################################################
+# Insert your solution below this line
+#######################################################
