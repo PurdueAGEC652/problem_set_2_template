d81464b4589cd74584ebbaa2709fefc52e1f3366
Diego S Cardoso <cardosod@purdue.edu>
2024-02-19 12:41:04 -0500

    Update ps2_q3.jl

--- ps2_q3.jl
+++ ps2_q3.jl
@@ -4,12 +4,12 @@ using Random
 Random.seed!(652) # Set a random seed for reproducibility
 
 # Set true parameter values
-β0_true = 2
-β1_true = 3
+β0_true = 2.0
+β1_true = 3.0
 
 # Data Generation
 n = 1000
-x = rand(n) * 10  # Uniform distribution
+x = rand(n) * 10.0  # Uniform distribution
 ϵ = randn(n)  # Normally distributed errors
 y = β0_true .+ β1_true .* x .+ ϵ
 
